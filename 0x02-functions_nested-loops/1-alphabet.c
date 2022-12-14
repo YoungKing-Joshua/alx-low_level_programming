5c78090609e48630f1e1f1862d4e7f99a2495835
JoshuaDei-Alorse <joshuadeialorse@gmail.com>
2022-12-13 18:51:56 -0800

    C functions

---
+++ 0x02-functions_nested-loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ *
+ * Return: Always 0(success)
+ */
+void print_alphabet(void)
+{
+	char apha;
+
+	for (apha = 'a'; apha <= 'z'; apha++)
+	{
+		_putchar(apha)
+	}
+
+	_putchar(\n)
+
+	return (0);
+}
