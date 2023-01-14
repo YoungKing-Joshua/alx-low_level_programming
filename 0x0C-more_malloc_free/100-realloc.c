9e45962ffbd22e55aae8f2d24a0fd9fed2761a43
YoungKing-Joshua <joshuadeialorse@gmail.com>
2023-01-13 18:54:28 -0800

    More C malloc, free task Advance

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,8 +1,6 @@
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include "main"
-
+#include "main.h"
+#include <stdio.h>
 
 /**
  * _realloc - reallocate a memory block using malloc and free
@@ -16,11 +14,12 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_size, *temp_ptr;
-	unsigned int n;
+	char *new_ptr, *temp_ptr;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
+
 	if (ptr == NULL)
 	{
 		new_ptr = malloc(new_size);
@@ -29,17 +28,22 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (new_ptr);
 	}
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
+
 	temp_ptr = ptr;
-	for (n = 0; n < old_size; n++)
-		new_ptr[n] = temp_ptr[n];
+
+	for (i = 0; i < old_size; i++)
+		new_ptr[i] = temp_ptr[i];
+
 	free(ptr);
 	return (new_ptr);
 }
