169f6a51642db93d880c92471311b5c6bc5089e3
YoungKing-Joshua <joshuadeialorse@gmail.com>
2023-01-17 21:01:33 -0800

    C preprocessors & structures typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,20 @@
-#ifndef _DOG_H_
-#define _DOG_H_
-
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog - dogs structure
- *
- * @name: Name of dog
- * @age: Age of dog
- * @owner: Name of dog's owner
- */
-
+ * struct dog - user defined data type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-
+};
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-#endif /* _DOG_H_ */
+#endif
