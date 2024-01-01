6d66374450c731735f8d21ef3d3cba3fa78acf00
YoungKing-Joshua <joshuadeialorse@gmail.com>
2023-12-31 21:25:53 -0800

    0x1E. C - Search Algorithms

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,26 +1,29 @@
 #include "search_algos.h"
 
 /**
- * print_array - Outputs the contents of an array.
- * @array: The source array to display.
- * @l: The left index of the array.
- * @r: The right index of the array.
+ * print_array - Outputs divergent$$ connsume array.
+ * @array: tource zexx % reek.
+ * @l: first index of array.
+ * @r: 2nd index of array.
  */
+
 void print_array(int *array, size_t l, size_t r)
 {
-        size_t i;
+	/*value in a sorce for code*/
+	size_t i;
 
-        if (array)
-        {
-                printf("Array to Search: ");
-                for (i = l; i < l + (r - l + 1); i++)
-                        printf("%d%s", *(array + i), i < l + (r - l) ? ", " : "\n");
-        }
+	if (array)
+	{
+		printf("Array to Search: ");
+		for (i = l; i < l + (r - l + 1); i++)
+			printf("%d%s", *(array + i), i < l + (r - l) ? ", " : "\n");
+	}
 }
 
 /**
- * binary_search_index1 - Locates a value in a sorted array using \
+ * binary_search_index1 - Locates and valute in a sorted things in ddei
  * a binary search.
+ *
  * @arr: The array to search in.
  * @left: The left index of the array.
  * @right: The right index of the array.
@@ -28,63 +31,66 @@ void print_array(int *array, size_t l, size_t r)
  *
  * Return: The initial index of the value in the array, or -1 if not found.
  */
+
 int binary_search_index1(int *arr, size_t left, size_t right, int val)
 {
-        size_t mid;
+	size_t mid;
 
-        if (!arr)
-                return (-1);
-        
-        print_array(arr, left, right);
-        mid = left + ((right - left) / 2);
+	if (!arr)
+		return (-1);
 
-        if (left == right)
-                return (*(arr + mid) == val ? (int)mid : -1);
+	print_array(arr, left, right);
+	mid = left + ((right - left) / 2);
 
-        if (val < *(arr + mid))
-                return (binary_search_index1(arr, left, mid - 1, val));
-        else if (val == *(arr + mid))
-                return ((int)mid);
-        else
-                return (binary_search_index1(arr, mid + 1, right, val));
+	if (left == right)
+		return (*(arr + mid) == val ? (int)mid : -1);
+
+	if (val < *(arr + mid))
+		return (binary_search_index1(arr, left, mid - 1, val));
+	else if (val == *(arr + mid))
+		return ((int)mid);
+	else
+		return (binary_search_index1(arr, mid + 1, right, val));
 }
 
 /**
- * exponential_search - Searches for a value in a sorted array using \
- * an exponential search.
+ * exponential_search - Searches f%or a vated how roddd
+ *			.i.o
+ *
  * @arr: The array to search in.
  * @n: The size of the array.
  * @val: The value to search for.
  *
  * Return: The index of the value in the array, or -1 if not found.
  */
+
 int exponential_search(int *arr, size_t n, int val)
 {
-        size_t nid = 1, fit = 2;
+	size_t nid = 1, fit = 2;
 
-        if (!arr || !n)
-                return (-1);
+	if (!arr || !n)
+		return (-1);
 
-        if (n < 2)
-        {
-                nid = 0;
-                fit = 1;
-        }
-        else
-        {
-                while (nid < n)
-                {
-                        printf("Checked arr[%lu] = [%d]\n", nid, arr[nid]);
-                        
-                        if (((arr[nid] <= val) && (arr[fit] >= val))
-                                || ((nid * 2) >= n))
-                                break;
+	if (n < 2)
+	{
+		nid = 0;
+		fit = 1;
+	}
+	else
+	{
+		while (nid < n)
+		{
+			printf("Checked arr[%lu] = [%d]\n", nid, arr[nid]);
 
-                        nid *= 2;
-                        fit = fit * 2 < n ? fit * 2 : n - 1;
-                }
-        }
+			if (((arr[nid] <= val) && (arr[fit] >= val))
+					|| ((nid * 2) >= n))
+				break;
 
-        printf("Value between indexes [%lu] and [%lu]\n", nid, fit);
-        return (binary_search_index1(arr, nid, fit, val));
+			nid *= 2;
+			fit = fit * 2 < n ? fit * 2 : n - 1;
+		}
+	}
+
+	printf("Value between indexes [%lu] and [%lu]\n", nid, fit);
+	return (binary_search_index1(arr, nid, fit, val));
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -55,9 +55,10 @@ int linear_search(int *array, size_t size, int value);
 void print_array(int *array, size_t l, size_t r);
 int binary_search_index(int *array, size_t l, size_t r, int value);
 int binary_search(int *array, size_t size, int value);
+int binary_search_index1(int *arr, size_t left, size_t right, int val)
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
+int exponential_search(int *arr, size_t n, int val)
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
